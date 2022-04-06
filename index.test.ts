0959db5b7b3b20872ce0023263ff27d2db9e5548
lby1024 <lby9090900@gmail.com>
2022-04-07 00:17:08 +0800

    feat: 添加了一些api

--- index.test.ts
+++ index.test.ts
@@ -6,137 +6,125 @@ import Promise from "./src/index";
 chai.use(sinonChai);
 const assert = chai.assert;
 
-describe("Promise", () => {
-  it("测试resolve", (done) => {
+describe("promise 测试", () => {
+  it("then resolve", (done) => {
+    new Promise((resolve) => resolve(1)).then((res) => {
+      assert(res === 1);
+      done();
+    });
+  });
+  it("then reject", (done) => {
+    new Promise((s, j) => j(1)).then(null, (res) => {
+      assert(res === 1);
+      done();
+    });
+  });
+  it("then 多次调用 resolve", (done) => {
+    let result = 0;
     new Promise((resolve) => {
-      setTimeout(() => resolve(200));
-    }).then((data) => {
-      assert(data === 200);
-      done();
-    });
-  });
-
-  it("测试reject", (done) => {
-    new Promise((resolve, reject) => {
-      setTimeout(() => reject(404));
-    }).then(null, (data) => {
-      assert(data === 404);
-      done();
-    });
-  });
-
-  it("多次执行 resolve()", (done) => {
-    const fn = sinon.fake();
-    new Promise((resolve) => {
-      resolve(200);
-      resolve(201);
-      resolve(202);
-    }).then(fn);
+      resolve(1);
+      resolve(2);
+      resolve(3);
+    }).then((res) => (result = res));
     setTimeout(() => {
-      assert(fn.calledOnce);
-      assert(fn.calledWith(200));
+      assert(result === 1);
       done();
     });
   });
-
-  it("多次执行 reject()", (done) => {
-    const fn = sinon.fake();
-    new Promise((resolve, reject) => {
-      reject(404);
-      reject(405);
-      reject(406);
-    }).then(null, fn);
+  it("then 多次调用", (done) => {
+    let result = "";
+    let p = new Promise((resolve) => resolve(1));
+    p.then(() => (result += "a"));
+    p.then(null, () => (result += "b"));
+    p.then(() => (result += "c"));
     setTimeout(() => {
-      assert(fn.calledOnce);
-      assert(fn.calledWith(404));
+      assert(result === "ac");
       done();
     });
   });
-
-  it("then 多次调用, 使用 resolve 触发", (done) => {
-    const p = new Promise((resolve) => resolve());
-    const callbacks = [sinon.fake(), sinon.fake()];
-    p.then(callbacks[0]);
-    p.then(callbacks[1]);
-    setTimeout(() => {
-      assert(callbacks[0].called);
-      assert(callbacks[1].called);
-      assert(callbacks[1].calledAfter(callbacks[0]));
-      done();
-    });
-  });
-
-  it("then 多次调用, 使用 reject 触发", (done) => {
-    const p = new Promise((resolve, reject) => reject());
-    const callbacks = [sinon.fake(), sinon.fake()];
-    p.then(null, callbacks[0]);
-    p.then(null, callbacks[1]);
-    setTimeout(() => {
-      assert(callbacks[0].called);
-      assert(callbacks[1].called);
-      assert(callbacks[1].calledAfter(callbacks[0]));
-      done();
-    });
-  });
-
-  it("执行then()返回一个promise对象", () => {
-    const t = new Promise(() => {}).then();
-    assert(t instanceof Promise);
-  });
-
-  it("链式调用, then(fn), fn返回一个字符串", (done) => {
-    const promise = new Promise((resolve) => resolve());
-    promise
-      .then(() => "成功")
-      .then((result) => {
-        assert.equal(result, "成功");
-        done();
-      });
-  });
-
-  it("链式调用, then(null, fn), fn返回一个字符串", (done) => {
-    const promise = new Promise((resolve, reject) => reject());
-    promise
-      .then(null, () => "成功")
-      .then((result) => {
-        assert.equal(result, "成功");
-        done();
-      });
-  });
-
-  it("链式调用, then(fn), fn返回一个 resolve 的 promise", (done) => {
-    new Promise((resolve) => resolve())
-      .then(() => new Promise((resolve) => resolve(1)))
+  it("then 链式调用", (done) => {
+    new Promise((s, j) => j("a"))
+      .then(null, (res) => res + "b")
+      .then((res) => res + "c")
       .then((res) => {
-        assert(res === 1);
+        return new Promise((resolve) => resolve(res + "d"));
+      })
+      .then((res) => new Promise((s, j) => j(res + "e")))
+      .then(null, (reason) => {
+        assert(reason === "abcde");
         done();
       });
   });
-
-  it("链式调用, then(fn), fn返回一个 reject 的 promise", (done) => {
-    new Promise((resolve) => resolve())
-      .then(() => new Promise((resolve, reject) => reject(1)))
-      .then(null, (res) => {
-        assert(res === 1);
+  it("then 透传", (done) => {
+    new Promise((s, j) => j("aaa"))
+      .then((res) => "xxx")
+      .then(null, (err) => {
+        assert(err === "aaa");
         done();
       });
   });
-
-  it("链式调用, then(null, fn), fn返回一个 resolve 的 promise", (done) => {
-    new Promise((resolve, reject) => reject())
-      .then(null, () => new Promise((resolve) => resolve(1)))
+  it("catch", (done) => {
+    new Promise((s, j) => j("a"))
+      .then(() => "xxx")
+      .catch((err) => err + "b")
+      .then((res) => res + "c")
       .then((res) => {
-        assert(res === 1);
+        return new Promise((resolve) => resolve(res + "d"));
+      })
+      .then((res) => new Promise((s, j) => j(res + "e")))
+      .catch((err) => {
+        assert(err === "abcde");
         done();
       });
   });
-
-  it("链式调用, then(null, fn), fn返回一个 reject 的 promise", (done) => {
-    new Promise((resolve, reject) => reject())
-      .then(null, () => new Promise((resolve, reject) => reject(1)))
-      .then(null, (res) => {
-        assert(res === 1);
-        done();
-      });
+  it("静态方法 resolve", (done) => {
+    Promise.resolve(1).then((res) => {
+      assert(res === 1);
+      done();
+    });
+  });
+  it("静态方法 reject", (done) => {
+    Promise.reject(1).then(null, (reason) => {
+      assert(reason === 1);
+      done();
+    });
+  });
+  it("静态方法 race resolve", (done) => {
+    const p1 = new Promise((resolve) => resolve(1));
+    const p2 = new Promise((resolve) => {
+      setTimeout(() => resolve(2), 10);
+    });
+    Promise.race([p1, p2]).then((res) => {
+      assert(res === 1);
+      done();
+    });
+  });
+  it("静态方法 race reject", (done) => {
+    const p1 = new Promise((resolve, reject) => reject(1));
+    const p2 = new Promise((resolve) => {
+      setTimeout(() => resolve(2), 10);
+    });
+    Promise.race([p1, p2]).then(null, (res) => {
+      assert(res === 1);
+      done();
+    });
+  });
+  it("静态方法 all resolve", (done) => {
+    const p1 = new Promise((resolve) => resolve(1));
+    const p2 = new Promise((resolve) => {
+      setTimeout(() => resolve(2), 10);
+    });
+    Promise.all([p1, p2]).then((res) => {
+      expect([1, 2]).toEqual(res);
+      done();
+    });
+  });
+  it("静态方法 all reject", (done) => {
+    const p1 = new Promise((resolve) => resolve(1));
+    const p2 = new Promise((s, j) => j(2));
+    Promise.all([p1, p2]).then(null, (res) => {
+      assert(res === 2);
+      done();
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,50 +1,93 @@
-type Callback = null | undefined | ((data: any) => any);
 type State = "pending" | "success" | "fail";
+type CB = null | undefined | ((data?: any) => any);
+type QueueItem = [CB, CB, Promise2];
 type Fn = (resolve: Promise2["resolve"], reject: Promise2["reject"]) => any;
 
 export default class Promise2 {
+  queue: QueueItem[] = [];
   state: State = "pending";
-  queue: [Callback, Callback, Promise2][] = [];
 
   constructor(fn: Fn) {
     fn.call(undefined, this.resolve.bind(this), this.reject.bind(this));
   }
 
-  resolve(data: any) {
-    this.sOrJ("success", data);
-  }
-
-  reject(data: any) {
-    this.sOrJ("fail", data);
-  }
-
-  then(onresolve: Callback, onreject: Callback) {
-    const p = new Promise2(() => {});
-    this.queue.push([onresolve, onreject, p]);
-    return p;
-  }
-
-  sOrJ(state: State, data: any) {
+  resolveOrReject(state: State, data: any) {
     setTimeout(() => {
       if (this.state !== "pending") return;
       this.state = state;
       const index = state === "success" ? 0 : 1;
       this.queue.forEach((item) => {
-        if (!item[index]) return;
-        const x = item[index]?.call(undefined, data);
-        this.resolveX(x, item[2]);
+        if (item[index] instanceof Function) {
+          const x = item[index]?.call(undefined, data);
+          item[2].resolveX(x);
+        } else {
+          if (state === "success") item[2].resolve(data);
+          if (state === "fail") item[2].reject(data);
+        }
       });
     });
   }
 
-  resolveX(x: any, p: Promise2) {
+  resolveX(x: any) {
     if (x instanceof Promise2) {
       x.then(
-        (res) => p.resolve(res),
-        (reason) => p.reject(reason)
+        (res) => this.resolve(res),
+        (reason) => this.reject(reason)
       );
       return;
     }
-    p.resolve(x);
+    this.resolve(x);
+  }
+
+  resolve(data: any) {
+    this.resolveOrReject("success", data);
+  }
+
+  reject(data: any) {
+    this.resolveOrReject("fail", data);
+  }
+
+  then(onResolve?: CB, onReject?: CB) {
+    const p = new Promise2(() => {});
+    this.queue.push([onResolve, onReject, p]);
+    return p;
+  }
+
+  catch(onReject?: CB) {
+    return this.then(null, onReject);
+  }
+
+  static resolve(data: any) {
+    return new Promise2((resolve) => resolve(data));
+  }
+
+  static reject(data: any) {
+    return new Promise2((resolve, reject) => reject(data));
+  }
+
+  static race(arr: Promise2[]) {
+    return new Promise2((resolve, reject) => {
+      arr.forEach((p) =>
+        p.then(
+          (res) => resolve(res),
+          (reason) => reject(reason)
+        )
+      );
+    });
+  }
+
+  static all(arr: Promise2[]) {
+    return new Promise2((resolve, reject) => {
+      let resArr: Promise2[] = [];
+      arr.forEach((p) => {
+        p.then(
+          (res) => {
+            resArr.push(res);
+            if (resArr.length === arr.length) resolve(resArr);
+          },
+          (reason) => reject(reason)
+        );
+      });
+    });
   }
 }
